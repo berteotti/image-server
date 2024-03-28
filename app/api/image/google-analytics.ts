2bf3721585337b09cffe02806b80fa2c9c0ccc6a
Berteotti <berteotti2@gmail.com>
2024-03-28 00:29:19 +0000

    fix private key

--- app/api/image/google-analytics.ts
+++ app/api/image/google-analytics.ts
@@ -6,7 +6,7 @@ const propertyId = process.env.GA_PROPERTY_ID;
 const analyticsDataClient = new BetaAnalyticsDataClient({
   credentials: {
     client_email: process.env.GA_CLIENT_EMAIL,
-    private_key: process.env.GA_PRIVATE_KEY?.replace(/\n/gm, "\n"), // replacing is necessary
+    private_key: process.env.GA_PRIVATE_KEY?.split(String.raw`\n`).join("\n"), // replacing is necessary
   },
 });
 
